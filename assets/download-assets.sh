19b08804a608a07b534b258cb78fb369333e07d6
Andrew Anderson <andrewloveskitties@gmail.com>
2020-05-03 15:21:50 -0700

    Use curl instead of wget

--- assets/download-assets.sh
+++ assets/download-assets.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget https://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c1.png -O front.png
-wget https://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c2.png -O back.png
+curl https://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c1.png -o front.png
+curl https://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c2.png -o back.png
