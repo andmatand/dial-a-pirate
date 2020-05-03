6984d82ae2c6963fba1c29e5feab431a3e632be0
Andrew Anderson <andrewloveskitties@gmail.com>
2020-05-03 15:21:08 -0700

    Use https in URLs

--- assets/download-assets.sh
+++ assets/download-assets.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget http://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c1.png -O front.png
-wget http://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c2.png -O back.png
+wget https://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c1.png -O front.png
+wget https://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c2.png -O back.png
